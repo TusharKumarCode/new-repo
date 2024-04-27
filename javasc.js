7123f34a657a2cadb94c2fb824395b390deb61be
Tushar Kumar <tbehera188@gmail.com>
2024-04-27 13:51:08 +0530

    modify code

--- javasc.js
+++ javasc.js
@@ -7,3 +7,21 @@ window.addEventListener('scroll',function(){
          }
          
 })
+
+const dropdn=document.querySelector("#hov");
+const list=document.querySelector("#setpos");
+dropdn.addEventListener('mouseover',function(){
+               list.setAttribute('class','d-flex justify-content-between align-items-center bg-secondary border border-0 rounded-3 d-block')
+})
+
+dropdn.addEventListener('mouseout',function(){
+    list.setAttribute('class','d-flex justify-content-between align-items-center bg-secondary border border-0 rounded-3 d-none')
+})
+
+list.addEventListener('mouseover',function(){
+    list.setAttribute('class','d-flex justify-content-between align-items-center bg-secondary border border-0 rounded-3 d-block')
+})
+
+list.addEventListener('mouseout',function(){
+    list.setAttribute('class','d-flex justify-content-between align-items-center bg-secondary border border-0 rounded-3 d-none')
+})
