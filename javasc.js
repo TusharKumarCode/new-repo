60991019fb200f13d7f89d84d16efe3483e2fa67
Tushar Kumar <tbehera188@gmail.com>
2024-05-29 16:25:57 +0530

    latest commit

--- javasc.js
+++ javasc.js
@@ -11,17 +11,24 @@ window.addEventListener('scroll',function(){
 const dropdn=document.querySelector("#hov");
 const list=document.querySelector("#setpos");
 dropdn.addEventListener('mouseover',function(){
-               list.setAttribute('class','d-flex justify-content-between align-items-center bg-secondary border border-0 rounded-3 d-block')
+               list.setAttribute('class','d-flex justify-content-between align-items-center border border-0 rounded-3 d-block')
 })
 
 dropdn.addEventListener('mouseout',function(){
-    list.setAttribute('class','d-flex justify-content-between align-items-center bg-secondary border border-0 rounded-3 d-none')
+    list.setAttribute('class','d-flex justify-content-between align-items-center border border-0 rounded-3 d-none')
 })
 
 list.addEventListener('mouseover',function(){
-    list.setAttribute('class','d-flex justify-content-between align-items-center bg-secondary border border-0 rounded-3 d-block')
+    list.setAttribute('class','d-flex justify-content-between align-items-center border border-0 rounded-3 d-block')
 })
 
 list.addEventListener('mouseout',function(){
-    list.setAttribute('class','d-flex justify-content-between align-items-center bg-secondary border border-0 rounded-3 d-none')
-})
+    list.setAttribute('class','d-flex justify-content-between align-items-center border border-0 rounded-3 d-none')
+});
+
+btn="";
+str=document.querySelector('#st');
+for(let i=1 ; i<=100; i++){
+    btn+='<button type="button" class="btn btn-primary w-25 mx-5 my-5 fs-2">ok</button>'
+}
+str.innerHTML=btn;
