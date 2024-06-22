161fc309ea0f8778f39485ff88958bfb2b51bef1
Tushar Kumar <tbehera188@gmail.com>
2024-06-22 17:47:53 +0530

    recent commit

--- javass.js
+++ javass.js
@@ -1,7 +1,14 @@
 let bticon=document.getElementById("bar");
 let closebtn=document.getElementById("cross");
 let slt=document.getElementById("colps");
+let remo=document.getElementById("btmo");
 
 bticon.addEventListener('click', function(){
            slt.setAttribute("class","flex justify-evenly md:w-3/4 items-center md:static absolute top-0 left-0 flex-col md:flex-row md:bg-inherit bg-orange-300 w-full h-full");
+           remo.setAttribute('class','md:w-64 md:mr-2 flex justify-evenly md:static absolute top-4 left-0 w-full')
 });
+
+closebtn.addEventListener("click",function(){
+    slt.setAttribute("class","flex justify-evenly md:w-3/4 items-center md:static absolute top-0 left-full flex-col md:flex-row md:bg-inherit bg-orange-300 w-full h-full");
+    remo.setAttribute('class','md:w-64 md:mr-2 flex justify-evenly md:static absolute top-4 left-full w-full')
+})
