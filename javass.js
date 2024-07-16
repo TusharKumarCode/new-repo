490c55f14602bbe291fc1ffc36086428b5fd768e
Tushar Kumar <tbehera188@gmail.com>
2024-07-16 17:03:53 +0530

    logcommit

--- javass.js
+++ javass.js
@@ -8,3 +8,14 @@ opbtn.addEventListener('click',function(){
 clbtn.addEventListener('click',function(){
     box.setAttribute('class',"md:flex flex justify-evenly items-center md:my-7 md:w-[1100px] w-full  md:h-auto absolute top-0 left-0 md:static flex-col md:flex-row bg-orange-400 md:bg-inherit hidden")
 });
+
+let sibtn=document.getElementById("sign");
+let sipage=document.getElementById("page");
+sibtn.addEventListener('click',function(){
+        sipage.setAttribute('class','bg-[rgba(0,0,0,0.6)] h-screen absolute top-0 w-full');
+});
+
+let closebtn=document.getElementById('cross');
+closebtn.addEventListener('click',function(){
+         sipage.setAttribute('class','bg-[rgba(0,0,0,0.6)] h-screen absolute top-[-700px] w-full')
+})
